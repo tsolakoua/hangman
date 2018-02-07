9becc44e7bf3b7011cf3d5b3f40f0f0f700bc53c
Anna Tsolakou <tsolakou.a@gmail.com>
2018-02-07 10:22:12 +0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,37 @@
 import random
 
-words_pool = ("dog", "cat", "programming", "school", "car")
-lives = 5
-word = random.choice(words_pool)
-print (word[0]+((len(word)-2)*"_")+word[-1])
+def create_table(word):
+		return list((word[0]+((len(word)-2)*"_")+word[-1]))
+
+def generate_word():
+	words_pool = ("dog", "cat", "programming", "school", "car", "chinese", "food", "analysis", "structure")
+	return random.choice(words_pool)
+
+def play_game(word, table, lives): 
+	print (table)
+	while lives > 0:
+		print ("Tries left: ", lives)
+		print (table)
+		guess = input()
+		missed_letter = 0
+		for i in range(len(word)):
+			if guess == word[i]:
+				table.pop(i)
+				table.insert(i, guess)
+				if "_" not in table:
+					print(word)
+					print("You won!")
+					break
+			else: 
+				missed_letter = missed_letter + 1
+				if missed_letter == len(word):
+					lives = lives - 1
+					if lives == 0:
+						print ("You Lost...")
+						print (("The word is ")+ word)
+						break
+
+lives = random.randint(5, 8)
+word = generate_word()
+table = create_table(word)
+play_game(word, table, lives)
